e5b804272ce8e511267fa0f71429abc447a73cae
Adrian Flores Herrera <adrian_fh_220699@outlook.com>
2026-09-24 13:43:40 -0600

    Persistent Local Data Storage Using UserDefaults

--- Todoey/AppDelegate.swift
+++ Todoey/AppDelegate.swift
@@ -19,6 +19,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         print("didFinishLaunchingWithOptions")
         
+        print(NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).last! as String)
+        
         return true
     }
 
--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -12,8 +12,16 @@ class TodoListViewController: UITableViewController {
     
     var itemArray = ["Find Mike", "Buy Eggos","Destroy Demogorgon"]
     
+    let defaults = UserDefaults.standard
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        if let  items = defaults.array(forKey: "TodoListArray") as? [String] {
+            
+            itemArray = items
+            
+        }
     
     }
     
@@ -29,6 +37,9 @@ class TodoListViewController: UITableViewController {
             // What will happen once the user clicks the Add Item Button on our UIAlert
 
             self.itemArray.append(textField.text!)
+            
+            self.defaults.set(self.itemArray, forKey: "TodoListArray")
+            
             self.tableView.reloadData()
             
         }
